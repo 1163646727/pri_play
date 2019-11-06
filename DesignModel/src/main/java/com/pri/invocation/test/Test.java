ccb6c7ca74e536e8477680a2dd9181b0c3ea5225
cq <1163646727@qq.com>
2019-11-06 20:02:26 +0800

    调整动态代理 的对象的命名

--- DesignModel/src/main/java/com/pri/invocation/test/Test.java
+++ DesignModel/src/main/java/com/pri/invocation/test/Test.java
@@ -15,22 +15,23 @@ import java.lang.reflect.Proxy;
 public class Test {
     public static void main(String[] args) {
        //被代理对象 ChenQi;
-        UserDaoImpl userDao = new UserDaoImpl ();
-        //代理对象 ChenQi;
-        InvocationHandlerImpl invocationHandler = new InvocationHandlerImpl (userDao);
+        UserDaoImpl userDaoImpl = new UserDaoImpl ();
+        //动态代理实例 ChenQi;
+        InvocationHandlerImpl invocationHandler = new InvocationHandlerImpl (userDaoImpl);
 
         //被代理对象 ChenQi;
-        ClassLoader classLoader = userDao.getClass ().getClassLoader ();
+        ClassLoader classLoader = userDaoImpl.getClass ().getClassLoader ();
 
         /**
-         * description: getInterfaces (),获取对象实现了哪些接口 <BR>
-         *     此处对象名是userDao，指向的对象是UserDaoImpl，UserDaoImpl实现了UserDao接口
+         * description: 被代理对象实现的接口集合<BR>
+         *     getInterfaces (),获取对象实现了哪些接口 <BR>
+         *     此处对象名是userDao，指向的对象是UserDaoImpl，UserDaoImpl实现了UserDao接口<BR>
          * author:  ChenQi <BR>
          * createDate:  2019-11-06 19:29  <BR>
          */
-        Class<?>[] interfaces = userDao.getClass ().getInterfaces ();
+        Class<?>[] interfaces = userDaoImpl.getClass ().getInterfaces ();
         //主要装载器，一组接口及调用处理动态代理实例
-        UserDao newProxyInstance = (UserDao) Proxy.newProxyInstance (classLoader,interfaces,invocationHandler);
-        newProxyInstance.add ("张三");
+        UserDao userDao = (UserDao) Proxy.newProxyInstance (classLoader,interfaces,invocationHandler);
+        userDao.add ("张三");
     }
 }
