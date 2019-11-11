55e7120bb6c697ba095529a763e1dde0bef763d0
cq <1163646727@qq.com>
2019-11-11 10:29:34 +0800

    CGLIB代理模式

---
+++ DesignModel/src/main/java/com/pri/proxy/cglib/CglibProxyTest.java
@@ -0,0 +1,25 @@
+package com.pri.proxy.cglib;
+
+import com.pri.proxy.BuyHouse;
+import com.pri.proxy.BuyHouseImpl;
+
+/**
+ * className:  CglibProxyTest <BR>
+ * description: CGLIB代理测试<BR>
+ * remark: <BR>
+ * author:  ChenQi <BR>
+ * createDate:  2019-11-11 09:48 <BR>
+ */
+public class CglibProxyTest {
+    public static void main(String[] args) {
+        System.out.println("正常访问--------------");
+        BuyHouse buyHouse = new BuyHouseImpl();
+        buyHouse.buyHouse();
+        System.out.println("静态代理访问--------------");
+        // 创建CGLIB代理对象 ChenQi;
+        CglibProxy cglibProxy = new CglibProxy();
+        // 创建代理对像 ChenQi;
+        BuyHouseImpl buyHouseCglibProxy = (BuyHouseImpl) cglibProxy.getInstance(buyHouse);
+        buyHouseCglibProxy.buyHouse();
+    }
+}
