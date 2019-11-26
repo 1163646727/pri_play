378d233c86140f5d14dfc7fcd9e0981c349258de
ChenQi <1163646727@qq.com>
2019-11-26 22:31:09 +0800

    同步代码

--- Ext_Spring/src/main/java/com/pri/mapper/UserMapper.java
+++ Ext_Spring/src/main/java/com/pri/mapper/UserMapper.java
@@ -5,7 +5,13 @@ import com.pri.aop.annotation.ExtParam;
 import com.pri.aop.annotation.ExtSelect;
 import com.pri.entity.User;
 
-
+/**
+ * interfaceName: UserMapper <BR>
+ * description: <BR>
+ * remark: <BR>
+ * author: ChenQi <BR>
+ * createDate: 2019-11-26 22:26 <BR>
+ */
 public interface UserMapper {
 
 	@ExtInsert("insert into user(userName,userAge) values(#{userName},#{userAge})")
--- Ext_Spring/src/test/java/Test0003.java
+++ Ext_Spring/src/test/java/Test0003.java
@@ -11,26 +11,23 @@ import com.pri.mapper.UserMapper;
  */
 public class Test0003 {
 
-	public static void main(String[] args) {/**
-	 *methodName:  main <BR>
-	 *description:  <BR>
-	 *remark: <BR>
-	 *param: args <BR>
-	 *return: void <BR>
-	 *author: ChenQi <BR>
-	 *createDate: 2019/8/8 0008 下午 10:57 <BR>
-	 */
-	
-	
-		// 使用动态代理技术虚拟调用方法
+	public static void main(String[] args) {
+/*		// 使用动态代理技术虚拟调用方法
 		UserMapper userMapper = SqlSession.getMapper(UserMapper.class);
 		User selectUser = userMapper.selectUser("张三", 30);
 		System.out.println(
 				"结果:" + selectUser.getUserName() + "," + selectUser.getUserAge() + ",id:" + selectUser.getId());
-		// // 先走拦截invoke
-		/*int insertUserResult = userMapper.insertUser("张三", 30);
-		System.out.println("insertUserResult:" + insertUserResult);*/
+
+		try {
+		} catch (Exception e) {
+		    System.out.println(e);
+		}*/
+		/* 获取当前目录位置 ChenQi*/
+		System.out.println(System.getProperty("user.dir"));//user.dir指定了当前的路径
+
+
 	}
-	
+
+
 
 }
