51fe6de2fe86aca3a771c86b86d55e3201714618
cq <1163646727@qq.com>
2019-12-11 15:53:03 +0800

    AOP底层实现：反射+动态代理

--- Ext_Spring/src/test/java/Test0003.java
+++ Ext_Spring/src/test/java/Test0003.java
@@ -12,7 +12,7 @@ import com.pri.mapper.UserMapper;
 public class Test0003 {
 
 	public static void main(String[] args) {
-/*		// 使用动态代理技术虚拟调用方法
+		// 使用动态代理技术虚拟调用方法
 		UserMapper userMapper = SqlSession.getMapper(UserMapper.class);
 		User selectUser = userMapper.selectUser("张三", 30);
 		System.out.println(
@@ -21,13 +21,7 @@ public class Test0003 {
 		try {
 		} catch (Exception e) {
 		    System.out.println(e);
-		}*/
-		/* 获取当前目录位置 ChenQi*/
-		System.out.println(System.getProperty("user.dir"));//user.dir指定了当前的路径
-
-
+		}
 	}
-
-
 
 }
