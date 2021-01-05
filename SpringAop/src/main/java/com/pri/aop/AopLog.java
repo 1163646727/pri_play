fbd3644ed780c91bfc535444c54082f4414b8071
ChenQi <1163646727@qq.com>
2021-01-05 16:02:40 +0800

    研究切面通知，如何获取类、方式

--- SpringAop/src/main/java/com/pri/aop/AopLog.java
+++ SpringAop/src/main/java/com/pri/aop/AopLog.java
@@ -1,5 +1,6 @@
 package com.pri.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
@@ -7,7 +8,12 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * className: AopLog <BR>
@@ -24,19 +30,45 @@ public class AopLog {
 	
 	//前置通知
 	@Before("execution(* com.pri.service.UserService.add(..))")
-	public void before(){
+	public void before(JoinPoint joinpoint) throws IllegalAccessException, InstantiationException, NoSuchMethodException {
+
+		// 获取代理目标对象
+		Class classTarget = joinpoint.getSignature().getDeclaringType();
+		// 获取目标对象类型
+		Class<?>[] par = ((MethodSignature)joinpoint.getSignature()).getParameterTypes();
+		// 获取代理目标对象的方法名称 ChenQi
+		String methodName = joinpoint.getSignature().getName();
+		Method objMethod = classTarget.getMethod(methodName,par);
+
+		// 获取方法的入参 ChenQi
+		List<Object> args = Arrays.asList(joinpoint.getArgs());
+		System.out.println("类名:"+ classTarget.getName() + ",方法:" + methodName+",参数："+args);
+
 		System.out.println("AopLog_前置通知 在方法之前执行...");
 	}
 	// 后置通知 在方法运行后执行
 	@After("execution(* com.pri.service.UserService.add(..))")
 	public void after() {
-	System.out.println("AopLog_后置通知 在方法之后执行...");
+		System.out.println("AopLog_后置通知 在方法之后执行...");
 	}
 	
 	// 异常通知
-	@AfterThrowing("execution(* com.pri.service.UserService.add(..))")
-	public void afterThrowing() {
-	System.out.println("AopLog_异常通知");
+	@AfterThrowing(value = "execution(* com.pri.service.UserService.add(..))",throwing = "ex")
+	public void afterThrowing(JoinPoint joinpoint,Exception ex) throws NoSuchMethodException {
+
+		// 获取代理目标对象
+		Class classTarget = joinpoint.getSignature().getDeclaringType();
+		// 获取目标对象类型
+		Class<?>[] par = ((MethodSignature)joinpoint.getSignature()).getParameterTypes();
+		// 获取代理目标对象的方法名称 ChenQi
+		String methodName = joinpoint.getSignature().getName();
+		Method objMethod = classTarget.getMethod(methodName,par);
+		// 获取方法的入参 ChenQi
+		List<Object> args = Arrays.asList(joinpoint.getArgs());
+		System.out.println("类名:"+ classTarget.getName() + ",方法:" + methodName+",参数："+args);
+
+		System.out.println("AopLog_异常通知->类名:"+ classTarget.getName() + ",方法:"
+				+ methodName+",参数："+args+";异常信息:"+(ex == null ? "NULL" : ex.getMessage()));
 	}
 	
 	// 环绕通知 在方法之前和之后处理事情
@@ -53,7 +85,7 @@ public class AopLog {
 	// 运行通知
 	@AfterReturning("execution(* com.pri.service.UserService.add(..))")
 	public void returning() {
-	System.out.println("AopLog_运行通知");
+		System.out.println("AopLog_运行通知");
 	}
 
 }
